579bc0a280d663e33baa248b48b0fee3b536390f
E02 <chewjob99@gmail.com>
2024-09-20 13:50:19 +0900

    중첩 클래스 활용
    중첩 클래스 활용
    이번 공부를 통해서 불필요한 클래스는 중첩 클래스를 통해 가독성을 높이고 그리고
    다른 사람이 봐도 헷갈리지 않게 하는 법은 배운것 같다.

--- src/nested/nested/NestedOuterMain.java
+++ src/nested/nested/NestedOuterMain.java
@@ -1,8 +1,5 @@
 package nested.nested;
 
-import java.time.LocalDate;
-import java.time.Period;
-import java.time.temporal.ChronoUnit;
 
 public class NestedOuterMain {
     public static void main(String[] args) {
@@ -12,13 +9,6 @@ public class NestedOuterMain {
 
         System.out.println("nestedClass" + nested.getClass());
         // nestedClass class nested.nested.NestedOuter$Nested 안쪽 클래스의 이름을 $로 구분한다.
-
-        LocalDate startDate = LocalDate.of(2024, 9, 20);
-        LocalDate endDate = LocalDate.of(2024, 10, 6);
-
-        Period between = Period.between(startDate, endDate);
-        long between1 = ChronoUnit.DAYS.between(startDate, endDate);
-        System.out.println("between1 = " + between1);
 
     }
 }
